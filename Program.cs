ec31345253c7b0d7cad7dd8e870d64e1dd877bf7
Rimolo13 <dornichmaximo@gmail.com>
2026-05-02 18:32:21 +0200

    Temp Fix for Routing

--- Program.cs
+++ Program.cs
@@ -36,7 +36,8 @@ namespace GZMaps
 
             // Razor Pages
             app.MapRazorPages();
-
+            app.MapFallbackToFile("/index.html");
+            
             // Controllers
             app.MapControllerRoute(
                 name: "default",
