6c61c9989349029e8d4e352b80be439f25e975ff
Rimolo13 <dornichmaximo@gmail.com>
2026-05-04 10:05:35 +0200

    Metal lives forever

--- Models/Password.cs
+++ Models/Password.cs
@@ -25,28 +25,6 @@ namespace GZMaps.Models
                 return new PasswordResult(false);
             }
 
-            /*
-            if(_passwordInput == "LemmyIsGod")
-            {
-                Process.Start(new ProcessStartInfo { FileName = "https://youtu.be/fM1UPeAOyHM?si=B5QVM3Gl32ScGb6w", UseShellExecute = true });
-            }
-        
-            if (_passwordInput == "PrinceOfFuckingDarkness")
-            {
-                Process.Start(new ProcessStartInfo { FileName = "https://youtu.be/S6A13bOB76A?si=bw6tJRjNZ_Gt4rA7", UseShellExecute = true });
-            }
-
-            if (_passwordInput == "ComeTouchMyMetalMachine")
-            {
-                Process.Start(new ProcessStartInfo { FileName = "https://youtube.com/shorts/mTQffwXF1k8?si=QN8msGXkStp0srmz", UseShellExecute = true });
-            }
-
-            if (_passwordInput == "MetalGods")
-            {
-                Process.Start(new ProcessStartInfo { FileName = "https://youtu.be/CLWlCQZy87g?si=nNuZagMzejflPyff", UseShellExecute = true });
-            }
-            */ 
-
             return new PasswordResult(_passwordInput == _storedPassword);
         }
     }
